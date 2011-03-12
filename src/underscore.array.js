fb562f00633144cdb9f11a9ebc2e68d7f45f530e
Michael Deardeuff <grignaak@gmail.com>
2011-03-11 20:51:37 -0800

    better directory structure. add jslint

---
+++ src/underscore.array.js
@@ -0,0 +1,80 @@
+/*jslint undef: true, browser: true, white: true, onevar: true, newcap: true, regexp: true, plusplus: true, bitwise: true, strict: true, maxerr: 50, indent: 2 */
+/*global window, define */
+(function () {
+  "use strict";
+  var _a = {
+    groupby: function (array, iterator, context) {
+      if (array.length === 0) {
+        return [];
+      }
+
+      var previous = iterator.call(context, array[0]),
+        currentPartition = [array[0]],
+        results = [currentPartition],
+        i,
+        current;
+
+      for (i = 1; i < array.length; i += 1) {
+        current = iterator.call(context, array[i]);
+        if (previous !== current) {
+          previous = current;
+          currentPartition = [];
+          results.push(currentPartition);
+        }
+        currentPartition.push(array[i]);
+      }
+      return results;
+    },
+
+    partition: function (array, iterator, context) {
+      var same = [],
+        different = [],
+        results = [same, different],
+        i,
+        value;
+
+      for (i = 0; i < array.length; i += 1) {
+        value = array[i];
+        if (iterator.call(context, value)) {
+          same.push(value);
+        } else {
+          different.push(value);
+        }
+      }
+
+      return results;
+    },
+    
+    binarySearch: function (array, iterator, context) {
+      if (array === null) {
+        return null;
+      }
+
+      var startIndex  = 0,
+        stopIndex = array.length - 1,
+        middle = Math.floor((stopIndex + startIndex) / 2),
+        comparison;
+
+      while (startIndex <= stopIndex) {
+        comparison = iterator.call(context, array[middle]);
+        if (comparison === 0) {
+          return array[middle];
+        } else if (comparison < 0) {
+          stopIndex = middle - 1;
+        } else {
+          startIndex = middle + 1;
+        }
+
+        middle = Math.floor((stopIndex - startIndex) / 2) + startIndex;
+      }
+      return null;
+    }
+  };
+
+  if (typeof this._ !== 'undefined') {
+    this._.mixin(_a);
+  } else {
+    this._ = _a;
+  }
+
+}());
